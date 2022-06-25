c6cd2fa79bcb0eb959d06265dae1e42ce3729313
samredai <43911210+samredai@users.noreply.github.com>
2022-06-24 18:23:06 -0700

    Revert to grid layout, add centered <section> with left and right margins

--- iceberg-theme/static/js/iceberg-theme.js
+++ iceberg-theme/static/js/iceberg-theme.js
@@ -1,14 +1,3 @@
-// jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
-});
-
 // Highlight the top nav as scrolling occurs
 $('body').scrollspy({
     target: '.navbar-fixed-top'
